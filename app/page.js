1382c7f98a878e56a61d2195125128beccd047d7
GOURAVSINGH19 <gouravsingh4495@gmail.com>
2025-09-01 21:51:38 +0530

    _init_

--- app/page.js
+++ app/page.js
@@ -2,7 +2,9 @@ import Home from "@/components/page";
 export default function page() {
   return (
     <div className="w-screen h-screen bg-black">
-      <div className="w-full h-full bg-black"></div>
+      <div className="w-full h-full bg-black flex flex-col justify-center items-center">
+        <h1 className="text-4xl text-white">Scroll down for more 👇🏻</h1>
+      </div>
       <Home />
     </div>
   );
